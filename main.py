d5e861a974fbb374c195d2fd94cb189af374b8d4
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2022-12-12 11:57:15 +1100

    Using functions, also has comments.

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
+"""
+
+call this function when it gets dark: It'll  display a skull on the microbit lights and play creepy music
+
+"""
+# Display a skull on the LEDs and play a creepy tune.
+def react_to_darkness():
+    global its_dark
+    its_dark = True
+    basic.show_icon(IconNames.SKULL)
+    music.set_tempo(140)
+    music.start_melody(music.built_in_melody(Melodies.DADADADUM),
+        MelodyOptions.ONCE)
+# Display the sun on the LEDs and play "here comes the sun" in a major key
+def react_to_light():
+    global its_dark
+    its_dark = False
+    # music.play_melody("A F G A A F F F", 240)
+    music.set_tempo(240)
+    music.start_melody(here_sun, MelodyOptions.ONCE)
+    # music.play_melody("A", 120)
+    basic.show_leds("""
+        . # . # .
+                # . # . #
+                . # # # .
+                # . # . #
+                . # . # .
+    """)
+"""
+
+Here Comes The Sun, by The Beatles as a series of notes.
+
+"""
+its_dark = False
+here_sun: List[str] = []
+here_sun = ["A", "F", "G", "A", "A", "F", "F", "F"]
+# The leds on the MicroBit can check roughly how much light is shining on it. The number it gives you can be between 0 (no light at all) and 255 (a lot of light!). I've set it to 100, a number recommended as a good starting point by the people who made the Microbit. If the dark mode is triggering in bright rooms, lower the number by 10 and try again. If dark mode won't trigger  even in rooms with the light off, you might want to raise it by 10, and try again. Keep going until you find the right level for you.
+DARKNESS = 100
 basic.show_icon(IconNames.YES)
-got_dark = False
-reacted_to_darkness = False
+# Figure out if it's dark or light by checking the light sensor.
+if input.light_level() < DARKNESS:
+    react_to_darkness()
+else:
+    react_to_light()
 
 def on_forever():
-    global got_dark
-    if input.light_level() < 100 and not got_dark:
-        got_dark = True
-        if got_dark:
-            pass
-    if input.light_level() >= 120 and got_dark:
-        got_dark  = False
-        
+    if input.light_level() < DARKNESS and not (its_dark):
+        react_to_darkness()
+    if input.light_level() >= DARKNESS + 20 and its_dark:
+        react_to_light()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,57 @@
+/**
+ * call this function when it gets dark: It'll  display a skull on the microbit lights and play creepy music
+ */
+// Display a skull on the LEDs and play a creepy tune.
+function react_to_darkness () {
+    its_dark = true
+    basic.showIcon(IconNames.Skull)
+    music.setTempo(140)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+}
+// Display the sun on the LEDs and play "here comes the sun" in a major key
+function react_to_light () {
+    its_dark = false
+    // music.play_melody("A F G A A F F F", 240)
+    music.setTempo(240)
+    music.startMelody(here_sun, MelodyOptions.Once)
+    // music.play_melody("A", 120)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . # . # .
+        `)
+}
+/**
+ * Here Comes The Sun, by The Beatles as a series of notes.
+ */
+let its_dark = false
+let here_sun: string[] = []
+here_sun = [
+"A",
+"F",
+"G",
+"A",
+"A",
+"F",
+"F",
+"F"
+]
+// The leds on the MicroBit can check roughly how much light is shining on it. The number it gives you can be between 0 (no light at all) and 255 (a lot of light!). I've set it to 100, a number recommended as a good starting point by the people who made the Microbit. If the dark mode is triggering in bright rooms, lower the number by 10 and try again. If dark mode won't trigger  even in rooms with the light off, you might want to raise it by 10, and try again. Keep going until you find the right level for you.
+let DARKNESS = 100
 basic.showIcon(IconNames.Yes)
-let got_dark = false
-let reacted_to_darkness = false
-basic.forever(function on_forever() {
-    
-    if (input.lightLevel() < 100 && !got_dark) {
-        got_dark = true
-        if (got_dark) {
-            
-        }
-        
+// Figure out if it's dark or light by checking the light sensor.
+if (input.lightLevel() < DARKNESS) {
+    react_to_darkness()
+} else {
+    react_to_light()
+}
+basic.forever(function () {
+    if (input.lightLevel() < DARKNESS && !(its_dark)) {
+        react_to_darkness()
     }
-    
-    if (input.lightLevel() >= 120 && got_dark) {
-        got_dark = false
+    if (input.lightLevel() >= DARKNESS + 20 && its_dark) {
+        react_to_light()
     }
-    
 })
