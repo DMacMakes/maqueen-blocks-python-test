2b28b7963d21016337fe172eec6c0a32014c4c36
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2022-12-12 12:31:10 +1100

    lowered darkness threshold so my laptop light could trigger bright mode

--- main.ts
+++ main.ts
@@ -39,7 +39,7 @@ here_sun = [
 "F"
 ]
 // The leds on the MicroBit can check roughly how much light is shining on it. The number it gives you can be between 0 (no light at all) and 255 (a lot of light!). I've set it to 100, a number recommended as a good starting point by the people who made the Microbit. If the dark mode is triggering in bright rooms, lower the number by 10 and try again. If dark mode won't trigger  even in rooms with the light off, you might want to raise it by 10, and try again. Keep going until you find the right level for you.
-let DARKNESS = 100
+let DARKNESS = 80
 basic.showIcon(IconNames.Yes)
 // Figure out if it's dark or light by checking the light sensor.
 if (input.lightLevel() < DARKNESS) {
@@ -51,7 +51,7 @@ basic.forever(function () {
     if (input.lightLevel() < DARKNESS && !(its_dark)) {
         react_to_darkness()
     }
-    if (input.lightLevel() >= DARKNESS + 20 && its_dark) {
+    if (input.lightLevel() >= DARKNESS + 10 && its_dark) {
         react_to_light()
     }
 })
